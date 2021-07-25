a5889ad0bf351fa6ffae2273c328a5e1975e2308
Patanasak Chunkhajorn <85662524+wateryphoenix@users.noreply.github.com>
2021-07-25 09:37:53 +0700

    Create Exercise4_Patanasak_Ch.py

---
+++ Exercise4_Patanasak_Ch.py
@@ -0,0 +1,9 @@
+FoundEng = 78
+GenBus = 85.2
+IntroComSys = 79
+ComPro = 95
+print("-- Your Score --")
+print("Foundation English                  =",FoundEng)
+print("Genaral Business                    =",GenBus)
+print("Introduction to Computer System     =",IntroComSys)
+print("Computer Programming                =",ComPro)
